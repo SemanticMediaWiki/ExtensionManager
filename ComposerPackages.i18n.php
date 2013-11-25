b961994c5867a1fe12b69bd68615a564ff79f0e2
mwjames <jamesin.hongkong.1@gmail.com>
2013-11-26 02:20:19 +0900

    Text change

--- ComposerPackages.i18n.php
+++ ComposerPackages.i18n.php
@@ -13,10 +13,10 @@ $messages = array();
  */
 $messages['en'] = array(
 	'special-listcomposerpackages'  => 'Composer packages',
-	'listcomposerpackages'          => 'Composer packages',
+	'listcomposerpackages' => 'Composer packages',
 	'composerpackages-desc' => 'Composer packages',
 	'composerpackages-intro' => 'Composer writes the list of the installed packages into a lock file. To update the packages, use the composer <code>update</code> command.',
-	'composerpackages-file-not-available' => '$1 can not be read for the current configuration or setup. Running <code>composer install</code> is requiered to display installed packages.',
+	'composerpackages-file-not-available' => 'The file ($1) is not available for the current configuration or setup. Running <code>composer install</code> from the MediaWiki directory is requiered in order to display installed packages.',
 	'composerpackages-table-header' => 'Installed packages',
 	'composerpackages-table-header-package' => 'Package',
 	'composerpackages-table-header-type' => 'Type',
