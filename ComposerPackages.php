3b74e18aa68a1ef036b51c32d882cfc93ea91b8b
mwjames <jamesin.hongkong.1@gmail.com>
2013-11-26 04:56:34 +0900

    Update composer install

--- ComposerPackages.php
+++ ComposerPackages.php
@@ -10,7 +10,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
 
 define( 'COMPOSERPACKAGES_VERSION', '0.1' );
 
-$wgExtensionCredits['other'][] = array(
+$GLOBALS['wgExtensionCredits']['other'][] = array(
 	'path'            => __FILE__,
 	'name'            => 'ComposerPackages',
 	'version'         => COMPOSERPACKAGES_VERSION,
@@ -25,14 +25,14 @@ $dir = __DIR__ . '/';
 $GLOBALS['wgExtensionMessagesFiles']['ComposerPackages']      = $dir . 'ComposerPackages.i18n.php';
 $GLOBALS['wgExtensionMessagesFiles']['ComposerPackagesAlias'] = $dir . 'ComposerPackages.alias.php';
 
-$wgAutoloadClasses['ComposerPackages\PackagesFile']        = $dir . '/src/ComposerPackages/PackagesFile.php';
-$wgAutoloadClasses['ComposerPackages\ArrayMapper']         = $dir . '/src/ComposerPackages/ArrayMapper.php';
-$wgAutoloadClasses['ComposerPackages\PackagesFileReader']  = $dir . '/src/ComposerPackages/PackagesFileReader.php';
-$wgAutoloadClasses['ComposerPackages\TextBuilder']         = $dir . '/src/ComposerPackages/TextBuilder.php';
-$wgAutoloadClasses['ComposerPackages\MessageBuilder']      = $dir . '/src/ComposerPackages/MessageBuilder.php';
+$GLOBALS['wgAutoloadClasses']['ComposerPackages\PackagesFile']        = $dir . '/src/ComposerPackages/PackagesFile.php';
+$GLOBALS['wgAutoloadClasses']['ComposerPackages\ArrayMapper']         = $dir . '/src/ComposerPackages/ArrayMapper.php';
+$GLOBALS['wgAutoloadClasses']['ComposerPackages\PackagesFileReader']  = $dir . '/src/ComposerPackages/PackagesFileReader.php';
+$GLOBALS['wgAutoloadClasses']['ComposerPackages\TextBuilder']         = $dir . '/src/ComposerPackages/TextBuilder.php';
+$GLOBALS['wgAutoloadClasses']['ComposerPackages\MessageBuilder']      = $dir . '/src/ComposerPackages/MessageBuilder.php';
 
-$wgAutoloadClasses['ComposerPackages\Specials\ListComposerPackages'] = $dir . '/src/ComposerPackages/Specials/ListComposerPackages.php';
+$GLOBALS['wgAutoloadClasses']['ComposerPackages\Specials\ListComposerPackages'] = $dir . '/src/ComposerPackages/Specials/ListComposerPackages.php';
 
 // Special page
-$wgSpecialPages['ListComposerPackages'] = 'ComposerPackages\Specials\ListComposerPackages';
+$GLOBALS['wgSpecialPages']['ListComposerPackages'] = 'ComposerPackages\Specials\ListComposerPackages';
 
--- README.md
+++ README.md
@@ -2,5 +2,23 @@ A simple extension that deploys a <code>Special:ListComposerPackages</code> page
 
 For more information about how to use MediaWiki and [Composer][composer], see [here][mwcomposer].
 
+## Installation
+The recommended way to install this extension is through `Composer`. Just add the following to the MediaWiki ``composer.json`` file and run the ``php composer.phar install/update`` command.
+
+```json
+{
+	"require": {
+		"mwjames/composer-packages": "dev-master"
+	},
+	"repositories": [
+		{
+			"type": "vcs",
+			"url":  "git@github.com:mwjames/composer-packages.git"
+		}
+	],
+	"minimum-stability" : "dev"
+}
+```
+
 [composer]: http://getcomposer.org/
 [mwcomposer]: https://www.mediawiki.org/wiki/Composer
