af0a4c68e41c795b31cf1d35ff806a9b4ab57793
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2015-02-21 10:42:45 +0100

    Update and rename i18n to i18n/ExtensionManager.i18n.alias.php
    * Move I18n alias from extension root
    * Provide German translation

---
+++ i18n/ExtensionManager.i18n.alias.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Aliases for the special pages
+ *
+ * @author mwjames
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'ListComposerPackages' => array( 'ListComposerPackages' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'ListComposerPackages' => array( 'Composer-Programmpaketliste' ),
+);
